9ac22a2fa84025fd5584f587b9f5cbe8741893e3
Rebecca Arantes <arantes.rbc@gmail.com>
2020-02-13 08:01:00 -0300

    adding state to application and using placeholder as fields mask

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
 
-export const FormInput = ({ label, placeholder, inputType }) => {
+export const FormInput = ({ label, placeholder, inputType, value }) => {
   return (
     <div className="form-input-container">
       <label className="input">{label}</label>
-      <input type={inputType} placeholder={placeholder}></input>
+      <input value={value} type={inputType} placeholder={placeholder}></input>
     </div>
   );
 };
--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import { FormInput } from "../FormInput";
 
-export const RegisterForm = () => (
-  <form>
-    <FormInput label="nome completo" />
-    <FormInput label="cidade" />
-    <FormInput inputType="email" label="email" />
-    <FormInput label="cpf" />
-    <FormInput inputType="number" label="telefone" />
-  </form>
-);
+export class RegisterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      city: "",
+      email: "",
+      cpf: "",
+      phone: ""
+    };
+  }
+  render() {
+    return (
+      <form>
+        <FormInput label="nome completo" value={this.state.name} />
+        <FormInput label="cidade" value={this.state.city} />
+        <FormInput
+          inputType="email"
+          label="email"
+          placeholder="email@email.com"
+          value={this.state.email}
+        />
+        <FormInput
+          label="cpf"
+          placeholder="000.000.000-00"
+          value={this.state.cpf}
+        />
+        <FormInput
+          label="telefone"
+          placeholder="(xx) xxxxx-xxxx"
+          value={this.state.phone}
+        />
+      </form>
+    );
+  }
+}
