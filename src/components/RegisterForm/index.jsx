758c3f61fcfd21eee6aea50be966e3b192c38f67
Rebecca Arantes <arantes.rbc@gmail.com>
2020-02-13 07:25:18 -0300

    removing unused component

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FormInput } from "../FormInput";
-import { RadioInput } from "../RadioInput";
 
 export const RegisterForm = () => (
   <form>
@@ -9,8 +8,5 @@ export const RegisterForm = () => (
     <FormInput inputType="email" label="email" />
     <FormInput label="cpf" />
     <FormInput inputType="number" label="telefone" />
-    <h4>se identifica com o gênero feminino?</h4>
-    <RadioInput id="yes" label="sim" value="0" />
-    <RadioInput id="no" label="não" value="1" />
   </form>
 );
